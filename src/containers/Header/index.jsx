63e994426c9842210a3cdc2f4c57d95e13754c54
rickywychoi <cchaff0105@gmail.com>
2021-02-16 11:40:47 -0800

    Added very basic header

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Header = (props) => {
     return (
-        <div>
+        // TODO: When styling, do not use inline-styling - This is demo purpose
+        <div style={{
+            width: "100%",
+            height: "80px",
+            border: "2px solid black"
+        }}>
             This is header
         </div>
     )
