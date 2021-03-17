b82f9ed6d3839246eeed4e2b435ac4170e21fb6a
yogiduzit <yverma2@my.bcit.ca>
2021-03-17 13:18:04 -0700

    Can create projects

--- src/routes/dashboard/index.jsx
+++ src/routes/dashboard/index.jsx
@@ -1,38 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import WithSidebar from "../../hoc/WithSidebar";
 import WithHeader from "../../hoc/WithHeader";
-import {Col, Container, Row} from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 import PreviousWeekCard from "../../components/dashboard/prevWeekCard.component";
 import ProjectCard from "../../components/projects/projectCard.component";
-import projectFakeData from '../projects/projectsFakeData.json'
+import { fetchAllProjects } from '../../api/Project';
 
-const Dashboard = (props) => {
-    return (
-        <div>
-            <Container>
-                <h1 className='px-4 font-weight-bold'>Previous Week</h1>
-                <Row>
-                    <Col md={7}>
-                        <PreviousWeekCard />
-                    </Col>
-                    <Col md={5}>
+const Dashboard = () => {
+  const [projects, setProjects] = useState([]);
 
-                    </Col>
-                </Row>
-                <h1 className='my-5 px-4 font-weight-bold'>Recent Projects</h1>
-                <Row>
-                    {
-                        projectFakeData.projects.map((p, index) => {
-                            return (
-                                <ProjectCard key={index} project={p} />
-                            )
-                        })
-                    }
+  useEffect(() => {
+    getData()
+  }, [])
 
-                </Row>
-            </Container>
-        </div>
-    )
+  const getData = async () => {
+    const { data } = await fetchAllProjects();
+    setProjects(data.projects);
+  };
+
+  return (
+    <div>
+      <Container>
+        <h1 className='px-4 font-weight-bold'>Previous Week</h1>
+        <Row>
+          <Col md={7}>
+            <PreviousWeekCard />
+          </Col>
+          <Col md={5}>
+
+          </Col>
+        </Row>
+        <h1 className='my-5 px-4 font-weight-bold'>Recent Projects</h1>
+        <Row>
+          {
+            projects.map((p, index) => {
+              return (
+                <ProjectCard key={index} project={p} />
+              )
+            })
+          }
+
+        </Row>
+      </Container>
+    </div>
+  )
 }
 
 export default WithSidebar(WithHeader(Dashboard));
