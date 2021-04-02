1d95bba3f0fb0530462d9c97c50a707441e529d8
yogiduzit <yverma2@my.bcit.ca>
2021-04-01 17:27:18 -0700

    Fixed routing

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -8,7 +8,7 @@ const Routes = {
     VIEW_PROJECT: '/project/:id',
     VIEW_SUB_WORK_PACKAGE: '/project/:id/wp/:wpId',
     TIMESHEET: '/timesheets',
-    TIMESHEET_CREATE: '/timesheets-create',
+    TIMESHEET_CREATE: '/timesheet-create',
     TIMESHEET_DETAIL: '/timesheet/:id',
     TIMESHEET_EDIT: '/timesheet-edit/:id',
     REPORT: '/report',
